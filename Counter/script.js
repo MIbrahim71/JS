e9cdab8674d9ecc193a5e3fe05c481253ffcb05e
Ib <116495254+MIbrahim71@users.noreply.github.com>
2023-11-07 13:28:51 +0000

    update

--- Counter/script.js
+++ Counter/script.js
@@ -1,40 +1,69 @@
 use: "strict";
 
-const counter = document.querySelector(".counter");
-const decrease = document.querySelector(".btn-dec");
-const reset = document.querySelector(".btn-reset");
-const increase = document.querySelector(".btn-inc");
+// const decrease = document.querySelector(".btn-dec");
+// const reset = document.querySelector(".btn-reset");
+// const increase = document.querySelector(".btn-inc");
 
 // Decrease
 
-decrease.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("Decrease");
+// decrease.addEventListener("click", function (e) {
+//   e.preventDefault();
 
-  counter.textContent--;
+//   counter.textContent--;
 
-  if (counter.textContent < 0) {
-    counter.style.color = "red";
-  }
-});
+//   if (counter.textContent < 0) counter.style.color = "red";
+// });
 
-// Reset
+// // Reset
 
-reset.addEventListener("click", function (e) {
-  e.preventDefault();
+// reset.addEventListener("click", function (e) {
+//   e.preventDefault();
 
-  counter.textContent = 0;
-  counter.style.color = "black";
-});
+//   counter.textContent = 0;
+//   counter.style.color = "black";
+// });
 
-// Increase
+// // Increase
 
-increase.addEventListener("click", function (e) {
-  e.preventDefault();
+// increase.addEventListener("click", function (e) {
+//   e.preventDefault();
 
-  counter.textContent++;
+//   counter.textContent++;
 
-  if (counter.textContent > 0) {
-    counter.style.color = "green";
-  }
+//   if (counter.textContent > 0) counter.style.color = "green";
+// });
+
+// REFACTOR
+
+let count = 0;
+
+const btns = document.querySelectorAll(".btn");
+const counter = document.querySelector(".counter");
+
+// Select all btns
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    console.log("e");
+
+    // Look at clicked btn's classList
+    const currentStyle = e.currentTarget.classList;
+
+    if (currentStyle.contains("dec")) {
+      count--;
+      console.log("dec");
+    }
+
+    if (currentStyle.contains("inc")) {
+      count++;
+      console.log("inc");
+    }
+
+    if (currentStyle.contains("reset")) count = 0;
+
+    if (count < 0) counter.style.color = "red";
+    if (count > 0) counter.style.color = "green";
+    if (count == 0) counter.style.color = "black";
+
+    counter.textContent = count;
+  });
 });
