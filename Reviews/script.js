8914a0e4e12dfb82eba9e352d787f0129328dfa8
Ib <116495254+MIbrahim71@users.noreply.github.com>
2023-11-08 00:52:16 +0000

    update

--- Reviews/script.js
+++ Reviews/script.js
@@ -5,14 +5,14 @@ const reviews = [
     id: 1,
     name: "Samin Hoque",
     job: "Apple CEO",
-    img: "9C1A4ED0-9778-4D6E-9DBA-F29317B36A9D_1_105_c.jpeg",
+    img: "IMG_1017.JPG",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
     name: "Hafizur Rahman",
-    job: "Cybersecurity Consultant",
-    img: "https://www.course-api.com/images/people/person-2.jpeg",
+    job: "McDonalds' Degree Apprentice",
+    img: "IMG_2328.jpg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
@@ -26,7 +26,7 @@ const reviews = [
     id: 4,
     name: "Akifur Rahman",
     job: "HMP Feltham resident",
-    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    img: "IMG_1015.PNG",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
@@ -45,7 +45,7 @@ let currentItem = 0;
 // Load initial review
 window.addEventListener("DOMContentLoaded", function () {
   const item = reviews[currentItem];
-  img.src = item.src;
+  img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
@@ -55,7 +55,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
 const showPerson = (person) => {
   const item = reviews[person];
-  img.src = item.src;
+  img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
